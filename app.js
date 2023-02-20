046a66e3ebc1dc341e63f7c65e76dfc3c4823441
Coco <respinozafuentes@gmail.com>
2023-02-20 20:51:44 -0300

    Add ejs to web site

--- app.js
+++ app.js
@@ -3,14 +3,40 @@ const
   bodyParser = require( "body-parser" ),
   app = express();
 
-app.get( "/", function( req, res ){
-  var today = new Date();
+app.set( "view engine", "ejs" );
 
-  if ( today.getDay() === 6 || today.getDay() === 0 ){
-    res.send("Yay, it's the weekend!");
-  } else {
-    res.sendFile( __dirname + "/index.html" );
-  }
+app.get( "/", function( req, res ){
+  var
+    today = new Date(),
+    day = "";
+
+  switch ( today.getDay() ) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log(" Error: current day is equal to " + today.getDate() );
+    }
+
+  res.render( "lists", { kinfOfDay: day });
 });
 
 app.listen( 3000, function(){
