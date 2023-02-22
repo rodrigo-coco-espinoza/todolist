dcd2ed4a105ac5bcd24c53fd878d8208c347ca98
Coco <respinozafuentes@gmail.com>
2023-02-21 21:33:12 -0300

    Se añade input para lista

--- app.js
+++ app.js
@@ -3,42 +3,33 @@ const
   bodyParser = require( "body-parser" ),
   app = express();
 
+let
+  items = [ "Lavarse los dientes", "Desayunar" ];
+
 app.set( "view engine", "ejs" );
+app.use( bodyParser.urlencoded( { exteneded: true }));
 
 app.get( "/", function( req, res ){
-  var
+  let
     today = new Date(),
-    day = "";
+    options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long"
+    },
+    day = today.toLocaleDateString( "en-US", options );
 
-  switch ( today.getDay() ) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log(" Error: current day is equal to " + today.getDate() );
-    }
+  res.render( "lists", { kinfOfDay: day, itemList: items });
+});
 
-  res.render( "lists", { kinfOfDay: day });
+app.post( "/", function( req, res ){
+  let
+    item = req.body.newItem;
+
+  items.push(item);
+  res.redirect( "/" );
 });
 
 app.listen( 3000, function(){
-  console.log("Server started on port 3000.");
+  console.log( "Server started on port 3000." );
 });
